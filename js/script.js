e8bc48144fa12c4abf56b94070286fbdd2af8ad0
JayChandlani <jaychandlanijay@gmail.com>
2022-11-19 22:11:26 +0530

    19nov

--- js/script.js
+++ js/script.js
@@ -21,10 +21,43 @@ navBar.forEach(a => {
 
 let li = document.querySelectorAll('.nav-link');
 let sec = document.querySelectorAll('section')
-    function activeMenu() {
-        let len = sec.length;
-        while (--len&&window.scrollY +500 <sec[len].offsetTop) {}
-        li.forEach(ltx => ltx.classList.remove('active'));
-        li[len].classList.add('active');}
-        activeMenu();
-        window.addEventListener('scroll',activeMenu);
+function activeMenu() {
+    let len = sec.length;
+    while (--len && window.scrollY + 500 < sec[len].offsetTop) { }
+    li.forEach(ltx => ltx.classList.remove('active'));
+    li[len].classList.add('active');
+}
+activeMenu();
+window.addEventListener('scroll', activeMenu);
+
+// Getting Data From Form And Saving to Google Sheets;
+document.getElementById('formsub').onsubmit = (e) => {
+    e.preventDefault();
+    const toastLiveExample = document.getElementById('liveToast')
+    let formdata = document.getElementById('formsub');
+    let data = new FormData(formdata)
+    const toast = new bootstrap.Toast(toastLiveExample);
+    if (formdata[0].value) {
+        document.getElementById('status_bg').classList.remove('change', 'valid')
+        document.getElementById('status').innerText = "Pending"
+        document.getElementById('innertext').innerText = "Sending, Please wait"
+        toast.show();
+        let request = new XMLHttpRequest();
+        request.open("POST", "https://script.google.com/macros/s/AKfycbztXbkkuypw00TpFkRwKwb7sD_NYneKqEPLG95sD7KSCkrV2sm7qaodCIbWpn5R4y_7/exec");
+        request.onreadystatechange = () => {
+            if (request.readyState === 4 && request.status === 200) {
+                document.getElementById('status_bg').classList.add('change')
+                document.getElementById('status').innerText = "Successfull"
+                document.getElementById('innertext').innerText = "Your message has been submitted"
+                toast.show();
+                formdata.reset();
+            }
+        }
+        request.send(data);
+    } else {
+        document.getElementById('status_bg').classList.add('valid')
+        document.getElementById('status').innerText = "Error"
+        document.getElementById('innertext').innerText = "Please enter some thing"
+        toast.show();
+    }
+}
